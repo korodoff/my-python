42c85a733103353b2431efb4a1cc7cca090f6210
korodoff <rudoffsutnga368@gmail.com>
2018-06-12 22:58:57 +0530

    second time commiting

--- dsg.py
+++ dsg.py
@@ -1,1 +1,30 @@
-print("din't i ever tell you the defination of insanity")
+#print anything on screen
+x=input("print anything you want on the screen:")
+print(x)
+
+#addition of two string
+x="LA"
+y="ME"
+z=x+y
+print(z)
+
+# enter 3 variable
+print("enter these three information")
+p=input("name:")
+q=int(input("phno:"))
+r=input("password:")
+print("%s %d %s"%(p,q,r))
+
+# print let get started
+print("ok now let's get started")
+
+#print following value using placeholders
+s="acadview"
+cource="python"
+fees="6000"
+print("welcome to "+s+" where we teach "+cource+" for only "+str(fees)+" rupees")
+
+# bonus question
+x=100000
+y="tony stark"
+print("%d %s"%(x,y,))
